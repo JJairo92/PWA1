0348059b46a7fb7fc989af4fc7e3277d84876e21
Jairo <JJEcu13@hotmail.com>
2013-09-16 09:22:07 -0400

    guessing game wasn't finished, form validation hasn't been started, and added day 5 classwork practice

---
+++ JuradoJairo/Wk2/goal4_assign_guess/js/main.js
@@ -0,0 +1,32 @@
+/**
+ * GUESSING GAME:
+ *
+ * Created By: Jairo Jurado
+ * Date: 09/13/2013
+ * 
+ * GUESSING GAME
+ */
+
+//Game variables
+(function ()
+	var number = 0;
+
+	var 
+
+
+	var playersGuess = 0;
+	var guessesRemaining = 3;
+	var guessesMade = 0;
+
+	var dom = {
+		input: document.querySelector("#input"),
+		output: document.querySelector("#output"),
+		button: document.querySelector("button")
+	};
+
+	// the random will be a decimal number so we multiply by 10, the 
+	// the 1 at the end says to start at 1
+
+	var magicNum = Math.floor(Math.random()*10 + 1);
+	
+})();
---
+++ Practice/Day 5/js/main.js
@@ -0,0 +1,18 @@
+(function() {
+	var animals = [{
+		name: "Dog",
+		color: "Red",
+		speed: [50,60,70]
+	},
+	{
+		name: "Tiger",
+		color: "Striped",
+		speed: [35, 66, 43]
+	}
+	];
+
+	console.log(animals[0].name, animals[0].color, animals[0].speed);
+
+	
+
+})();
