5dda7169f268931778014019ef59f3200c5c7202
Jairo <JJEcu13@hotmail.com>
2013-09-18 13:42:23 -0400

    added Day 7 classwork practice and deleted the temporary files in Week 3

---
+++ Practice/Day 7/js/main.js
@@ -0,0 +1,81 @@
+(function () {
+	console.log("Start");
+
+	/*var ships = [];
+
+	function Ship(speed, name) {
+		this.speed = speed; // "this" is like making a property public in actionscript
+		this.name = name; // name of spaceship
+		var speed = 21;
+
+		console.log("Ship created.", this.speed, this.name);
+		console.log(this);
+
+		function moveShip() {
+			//this.x += this.speed;
+			console.log(this);
+		};
+	};
+
+	Ship.prototype.move = function() {
+		var self = this;
+		console.log(this);
+
+		console.log()
+
+		initEngines();
+		checkFuel();
+
+		function initEngines() {
+			console.log("init");
+		};
+		function checkFuel() {
+			console.log("Check Fuel")
+		}
+	};
+
+	var eli = new Ship(5, "Kestrel");
+	eli.move();
+
+	Array.prototype.eat = function()
+
+	/*var russell = new Ship(50, "Russell");
+	var mike = new Ship(-4, "Mike");
+
+	ships.push(eli, russell, mike);*/
+
+	/*function Dog(color, name) {
+		this.color = color;
+		this.name = name;
+	};
+
+	Dog.prototype.bark = function() {
+		var self = this;
+		openMouth();
+		function openMouth() {
+			console.log("I opened my mouth.");
+			chew();
+		};
+
+		function chew() {
+			console.log(?,self; "is chewing");
+		};
+
+		var max = new Dog("black", "Max");
+		var bandit = new Dog("black", "Bandit");
+		max.bark();
+	};/
+
+	window.colg*/
+
+	var interval = setInterval();
+	var max = new Dog("black", "Max");
+	var banit = setInterval (dow Dog)
+
+	function update() {
+		var dice = ~(Math.random()*6+1);
+
+		dom.innerHTML=dice;
+	}
+
+})();
